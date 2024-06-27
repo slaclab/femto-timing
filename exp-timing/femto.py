08c7b1585aa8c6e564c6c800bfbd31ea7b9169e9
rlodico06 <138170352+rlodico06@users.noreply.github.com>
2024-06-27 14:48:28 -0700

    Move Flag Bug

--- exp-timing/femto.py
+++ exp-timing/femto.py
@@ -635,7 +635,6 @@ def date_time():
 def move_time_delay(P,L,T):
     """Takes the time of the most recent set time adjustment, returns the approximate delay that occurred before the time interval counter detected the change in time."""
     try:
-        move_flag = 0
         if abs(L.pc_diff) > 1e-6 or move_flag == 1: # Checks if phase motor set position has changed
             C = time_interval_counter(P)
             curr_time = C.get_time() # Current counter time
