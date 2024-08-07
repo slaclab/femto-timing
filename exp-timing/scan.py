5983c1513052d533c561fd7cd663fb249173de2b
rlodico06 <138170352+rlodico06@users.noreply.github.com>
2024-08-07 09:49:23 -0700

    Temporary Logging Disable

--- exp-timing/scan.py
+++ exp-timing/scan.py
@@ -3,14 +3,14 @@ import logging
 import sys
 from epics import caget, caput, cainfo, PV
 
-logging.basicConfig(
-                format='%(asctime)s - %(levelname)s - %(message)s',
-                style='%',
-                datefmt='%Y-%m-%d %H:%M',
-                level=logging.DEBUG,
-                filename=str('/reg/d/iocData/py-fstiming-XCS/iocInfo/scan.log'),
-                filemode='a',
-            )
+# logging.basicConfig(
+#                 format='%(asctime)s - %(levelname)s - %(message)s',
+#                 style='%',
+#                 datefmt='%Y-%m-%d %H:%M',
+#                 level=logging.DEBUG,
+#                 filename=str('/reg/d/iocData/py-fstiming-XCS/iocInfo/scan.log'),
+#                 filemode='a',
+#             )
 
 # setup PVs
 tgt_pv = 'LAS:FS4:VIT:FS_TGT_TIME'
@@ -26,10 +26,10 @@ wait_time = 5 # wait time in seconds b/t steps
 # print current tgt and ctr time
 tgt = tgt_time_pv.value
 print(tgt)
-logging.info('%s', tgt)
+# logging.info('%s', tgt)
 ctr = ctr_time_pv.value
 print(ctr)
-logging.info('%s', ctr)
+# logging.info('%s', ctr)
 
 # scan through ns steps
 for x in range(0, stop, step):
@@ -37,11 +37,11 @@ for x in range(0, stop, step):
     time.sleep(wait_time) # wait x sec to update
     # printout the values to see on the terminal 
     print(tgt_time_pv.value)
-    logging.info('%s', tgt_time_pv.value)
+    # logging.info('%s', tgt_time_pv.value)
     print(ctr_time_pv.value)
-    logging.info('%s', ctr_time_pv.value)
+    # logging.info('%s', ctr_time_pv.value)
 
 # write back orig tgt time 
 print(tgt)
-logging.info('%s', tgt)
+# logging.info('%s', tgt)
 caput(tgt_pv,tgt, wait=True)
