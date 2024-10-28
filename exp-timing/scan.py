6911044a3dd6eb52bc58a8f9da38ecbb1bd41c2e
rlodico06 <138170352+rlodico06@users.noreply.github.com>
2024-10-28 10:39:05 -0700

    Updated scan.py for Zone 2 Checkout

--- exp-timing/scan.py
+++ exp-timing/scan.py
@@ -17,10 +17,14 @@ tgt_pv = 'LAS:LHN:LLG2:02:PHASCTL:DELAY_SET'
 tgt_time_pv = PV('LAS:LHN:LLG2:02:PHASCTL:DELAY_SET')
 ctr_time_pv = PV('LAS:LHN:LLG2:02:PHASCTL:GET_TIC_NS')
 
+#target/counter time dictionaries
+tgt_time = dict()
+ctr_time = dict()
+
 # scanning parameters 
-stop = 10 # scan range ns  
-step = 1 # scan step interval in ns 
-direction = -1 # -1 for down, 1 for up 
+init_step = 0.05 # initial step size
+stop = 8 # total number of steps to take
+mult = 10 # multiplier by which to increase/decrease step size after each step
 wait_time = 5 # wait time in seconds b/t steps
 
 # print current tgt and ctr time
@@ -32,14 +36,26 @@ print(ctr)
 # logging.info('%s', ctr)
 
 # scan through ns steps
-for x in range(0, stop, step):
-    caput(tgt_pv,tgt+x*direction, wait=True)
+for x in range(0, stop):
+    if (x<(stop/2)):
+        caput(tgt_pv,tgt+init_step*(mult^x), wait=True) # step up with increasing step size
+    else:
+        caput(tgt_pv,tgt-init_step*(mult^(x-(stop/2))), wait=True) # step down with increasing step size
     time.sleep(wait_time) # wait x sec to update
-    # printout the values to see on the terminal 
-    print(tgt_time_pv.value)
-    # logging.info('%s', tgt_time_pv.value)
-    print(ctr_time_pv.value)
-    # logging.info('%s', ctr_time_pv.value)
+    tgt_time[x] = tgt_time_pv.value
+    ctr_time[x] = ctr_time_pv.value
+
+#print target/counter time values
+print('Target Times:')
+#logging.info('Target Times:')
+for x in range(0, stop): 
+    print(tgt_time[x]) # print out all target time values first
+    # logging.info('%s', tgt_time[x])
+print('Counter Times:')
+#logging.info('Counter Times:')
+for x in range(0, stop):
+    print(ctr_time[x]) # then print out all counter time values
+    # logging.info('%s', ctr_time[x])
 
 # write back orig tgt time 
 print(tgt)
