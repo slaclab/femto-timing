9fa5b00c51b59da29c223efd1e406e8470ba60b0
dvc-x <dvc@slac.stanford.edu>
2025-04-09 15:39:18 -0700

    Update time_tool.py

--- exp-timing/time_tool.py
+++ exp-timing/time_tool.py
@@ -1,7 +1,7 @@
 #time_tool.py
 import time
 import numpy as np
-import watchdog
+import watchdog3
 from psp.Pv import Pv
 import sys
 
@@ -10,78 +10,84 @@ class time_tool():
         if sys == 'FS11': # set up for new bay 1 laser
             print('starting FS11')
             self.delay = 0.1
-            pvname = 'XPP:TIMETOOL:TTALL'  # time tool array name
-            dev_base = 'LAS:FS11:VIT:'  
-            stagename = 'XPP:LAS:MMN:16'  # delay stage for time tool
+            TTALL_Name = 'XPP:TIMETOOL:TTALL'  # time tool array name
+            Dev_Base = 'LAS:FS11:VIT:'  
+            Stage_Name = 'XPP:LAS:MMN:16'  # delay stage for time tool
             ipmname = 'XPP:SB2:BMMON:SUM' # intensity profile monitor PV
         elif sys == 'FS14':  # set up FS14 system
-            print('starting FS14')
+            print('Starting FS14')
             self.delay = 0.1 # 1 second delay
-            pvname = 'TMO:TIMETOOL:TTALL'  # time tool array name
-            dev_base = 'LAS:FS14:VIT:'
-            stagename = 'LM1K4:COM_MP2_DLY1'  # delay stage for time tool
-            ipmname = 'EM2K0:XGMD:HPS:milliJoulesPerPulse' # intensity profile monitor PV
-            print('exit FS14 elif')
+            #TTALL_Name = 'TMO:TIMETOOL:TTALL'  # time tool array name
+            Dev_Base = 'LAS:FS14:VIT:'
+            #Stage_Name = 'LM1K4:COM_MP2_DLY1'  # delay stage for time tool
+            #ipmname = 'EM2K0:XGMD:HPS:milliJoulesPerPulse' # intensity profile monitor PV
+            
+            print('Borrow CXI PVs to monitor')
+            TTALL_Name = 'CXI:TT:01:TTALL' #time tool array name
+            Stage_Name = 'CXI:LAS:MMN:01'  # delay stage for time tool
+            ipmname = 'CXI:DG2:BMMON:SUM' # intensity profile monitor PV
+            
+            print('Exit FS14 elif')
         elif sys == 'XPP':  # set up xpp system
             print('starting XPP')
             self.delay = 0.1 # 1 second delay
-            pvname = 'XPP:TIMETOOL:TTALL'  # time tool array name
-            dev_base = 'LAS:FS3:VIT:'
-            stagename = 'XPP:LAS:MMN:16'  # delay stage for time tool
+            TTALL_Name = 'XPP:TIMETOOL:TTALL'  # time tool array name
+            Dev_Base = 'LAS:FS3:VIT:'
+            Stage_Name = 'XPP:LAS:MMN:16'  # delay stage for time tool
             ipmname = 'XPP:SB2:BMMON:SUM' # intensity profile monitor PV
         elif sys == 'XCS':  # set up xcs system
             print('starting XCS')
             self.delay = 0.1 # 1 second delay
-            pvname = 'XCS:TIMETOOL:TTALL'  # time tool array name
-            dev_base = 'LAS:FS4:VIT:'
-            stagename = 'XCS:LAS:MMN:01'  # delay stage for time tool
+            TTALL_Name = 'XCS:TIMETOOL:TTALL'  # time tool array name
+            Dev_Base = 'LAS:FS4:VIT:'
+            Stage_Name = 'XCS:LAS:MMN:01'  # delay stage for time tool
             ipmname = 'XCS:SB1:BMMON:SUM' # intensity profile monitor PV
         elif sys == 'MFX':  # set up xcs system
             print('starting MFX')
             self.delay = 0.1 # 1 second delay
-            pvname = 'MFX:TT:01:TTALL'  # time tool array name
-            dev_base = 'LAS:FS45:VIT:'
-            stagename = 'MFX:LAS:MMN:06'  # delay stage for time tool
+            TTALL_Name = 'MFX:TT:01:TTALL'  # time tool array name
+            Dev_Base = 'LAS:FS45:VIT:'
+            Stage_Name = 'MFX:LAS:MMN:06'  # delay stage for time tool
             ipmname = 'MFX:DG2:BMMON:SUM' # intensity profile monitor PV
         elif sys == 'CXI':  # set up cxi system
             print('starting CXI')
             self.delay = 0.1 # 1 second delay
-            pvname = 'CXI:TT:01:TTALL' #time tool array name
-            dev_base = 'LAS:FS5:VIT:'
-            stagename = 'CXI:LAS:MMN:01'  # delay stage for time tool
-            #stagename = 'CXI:USR:MMN:25'  # delay stage for time tool
+            TTALL_Name = 'CXI:TT:01:TTALL' #time tool array name
+            Dev_Base = 'LAS:FS5:VIT:'
+            Stage_Name = 'CXI:LAS:MMN:01'  # delay stage for time tool
+            #Stage_Name = 'CXI:USR:MMN:25'  # delay stage for time tool
             ipmname = 'CXI:DG2:BMMON:SUM' # intensity profile monitor PV
         else:
             print(sys+' not found, exiting')
             exit()
         
         print('Script PV 1')
-        self.tt_script_en = Pv(dev_base+'matlab:31')
-        self.tt_script_en.connect(timeout=1.0) # connect to pv
+        self.TT_Script_EN = Pv(Dev_Base+'matlab:31')
+        self.TT_Script_EN.connect(timeout=1.0) # connect to pv
         
-        self.ttpv = Pv(pvname)
-        self.ttpv.connect(timeout=1.0) # connect to pv
-        self.stagepv = Pv(stagename)
-        self.stagepv.connect(timeout=1.0)
-        self.ipmpv = Pv(ipmname)
-        self.ipmpv.connect(timeout=1.0)
+        self.TTALL_PV = Pv(TTALL_Name)
+        self.TTALL_PV.connect(timeout=1.0) # connect to pv
+        self.Stage_PV = Pv(Stage_Name)
+        self.Stage_PV.connect(timeout=1.0)
+        self.IPM_PV = Pv(ipmname)
+        self.IPM_PV.connect(timeout=1.0)
         self.drift_correct_pv = dict()  # will hold list of IOC pvs
         self.values = dict() # will hold the numbers from the time tool
         self.limits = dict() # will hold limits from matlab pvs
         self.old_values = dict() # will hold the old values read from matlab
         self.drift_correct = dict()
         self.nm = ['Watchdog', 'pix', 'fs', 'amp', 'amp_second', 'ref', 'FWHM', 'Stage', 'ipm', 'dcsignal', 'MATLAB10'] #list of internal names
-        self.drift_correct_pv[0] = dev_base+'WATCHDOG'
-        self.drift_correct_pv[1] = dev_base+'PIX'
-        self.drift_correct_pv[2] = dev_base+'FS'
-        self.drift_correct_pv[3] = dev_base+'AMP'
-        self.drift_correct_pv[4] = dev_base+'AMP_SEC'
-        self.drift_correct_pv[5] = dev_base+'REF'
-        self.drift_correct_pv[6] = dev_base+'FWHM'
-        self.drift_correct_pv[7] = dev_base+'STAGE'
-        self.drift_correct_pv[8] = dev_base+'IPM'
-        self.drift_correct_pv[9] = dev_base+'DRIFT_CORRECT_SIG'
-        self.drift_correct_pv[10] = dev_base+'matlab:10'
+        self.drift_correct_pv[0] = Dev_Base+'WATCHDOG'
+        self.drift_correct_pv[1] = Dev_Base+'PIX'
+        self.drift_correct_pv[2] = Dev_Base+'FS'
+        self.drift_correct_pv[3] = Dev_Base+'AMP'
+        self.drift_correct_pv[4] = Dev_Base+'AMP_SEC'
+        self.drift_correct_pv[5] = Dev_Base+'REF'
+        self.drift_correct_pv[6] = Dev_Base+'FWHM'
+        self.drift_correct_pv[7] = Dev_Base+'STAGE'
+        self.drift_correct_pv[8] = Dev_Base+'IPM'
+        self.drift_correct_pv[9] = Dev_Base+'DRIFT_CORRECT_SIG'
+        self.drift_correct_pv[10] = Dev_Base+'matlab:10'
 
         #print('Value of Watchdog'+self.drift_correct_pv[0])
         for n in range(0, len(self.nm)):
@@ -91,31 +97,33 @@ class time_tool():
             for x in range(0,3):
                 self.drift_correct[self.nm[n]][x].get(ctrl=True, timeout=1.0)
                 self.drift_correct[self.nm[n]][3].put(value = self.nm[n], timeout = 1.0)
-        self.W = watchdog.watchdog(self.drift_correct[self.nm[0]][0]) # initialize watchdog   
+        self.W = watchdog3.watchdog(self.drift_correct[self.nm[0]][0]) # initialize watchdog   
 
     def read_write(self):   
-        self.ttpv.get(ctrl=True, timeout=1.0) # get TT array data
-        self.stagepv.get(ctrl=True, timeout=1.0) # get TT stage position
+        self.TTALL_PV.get(ctrl=True, timeout=1.0) # get TT array data
+        self.Stage_PV.get(ctrl=True, timeout=1.0) # get TT stage position
         self.ipmpv.get(ctrl=True, timeout=1.0) # get intensity profile
 
-        self.tt_script_en.get(ctrl=True, timeout=1.0)
+    _    self.TT_Script_EN.get(ctrl=True, timeout=1.0)
 
         for n in range(1, len(self.nm)):
              self.old_values[self.nm[n]] = self.drift_correct[self.nm[n]][0].value # old PV values
              #self.limits[self.nm[n]] = [self.drift_correct[self.nm[n]][1].value, self.drift_correct[self.nm[n]][2].value] # limits
         if n in range (1,6):
-            self.drift_correct[self.nm[n]][0].put(value = self.ttpv.value[n-1], timeout = 1.0)  # write to matlab PVs 
+            self.drift_correct[self.nm[n]][0].put(value = self.TT_P
+V.value[n-1], timeout = 1.0)  # write to matlab PVs 
             for x in range(0,3):
                 self.drift_correct[self.nm[n]][x].get(ctrl=True, timeout=1.0)  # get all the matlab pvs
-        self.drift_correct[self.nm[7]][0].put(value = self.stagepv.value, timeout = 1.0)  # read stage position
+        self.drift_correct[self.nm[7]][0].put(value = self.Stage_PV.value, timeout = 1.0)  # read stage position
         self.drift_correct[self.nm[8]][0].put(value = self.ipmpv.value, timeout = 1.0) # read/write intensity profile
 
-         ###
-         #print self.ttpv.value
+    _     ###
+         #print self.TT_P
+V.value
          #print 'stage position' # TEMP
-         #print self.stagepv.value # TEMP
+         #print self.Stage_PV.value # TEMP
          #print 'intensity profile sum'
-         #print self.ipmpv.value
+         #print self.IPM_PV.value
 
          # need to decide whether to output to the drift correction signal
          # 1. IPM must be in range
@@ -140,9 +148,9 @@ class time_tool():
                  #print 'TT edge fit bad'
          #else:
              #print 'intensity profile bad'
-        if( self.tt_script_en.value == 1):
+        if( self.TT_Script_EN.value == 1):
             print('Do a correction!')
-            self.drift_correct[self.nm[10]][0].put(value = self.tt_script_en.value, timeout = 1.0)
+            self.drift_correct[self.nm[10]][0].put(value = self.TT_Script_EN.value, timeout = 1.0)
             #self.drift_correct_pv[0] = self.drift_correct_pv[0] + 1
             time.sleep(1)
         else:
