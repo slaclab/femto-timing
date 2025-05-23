754821080ed6e280024e0e05f04611809e5fefd2
rlodico06 <138170352+rlodico06@users.noreply.github.com>
2025-05-23 15:53:48 -0700

    Capped corrections at 1 ps

--- exp-timing/crixs_atm_fb.py
+++ exp-timing/crixs_atm_fb.py
@@ -58,7 +58,7 @@ class drift_correction():
                 self.ampl_vals[self.count] = self.ampl
                 self.error_vals[self.count] = self.flt_pos_ps
                 self.count+=1
-                print("Count is now: "+self.count)
+                print("Count is now: ", self.count)
         # averaging
         self.avg_ampl = sum(self.ampl_vals.values()) / len(self.ampl_vals)
         self.avg_error = sum(self.error_vals.values()) / len(self.error_vals)
@@ -68,10 +68,12 @@ class drift_correction():
         # apply correction
         self.fb_gain = self.fb_gain_pv.get(timeout = 1.0)  # pull gain PV value
         self.on_off = self.on_off_pv.get(timeout = 1.0)
-        if (self.on_off == 1):  # check if drift correction has been turned on
-            print("Correction turned on.")
-            self.atm_fb_pv.put(value = (self.avg_error/1000) * self.fb_gain, timeout = 1.0)  # scale from ps to ns, apply gain, and write to correction PV
-        
+        self.correction = (self.avg_error/1000) * self.fb_gain # scale from ps to ns and apply gain
+        if (self.on_off == 1):  # check if drift correction has been turned on and limit corrections to 1 ps
+            if ((abs(self.correction) < 0.001)): # cap corrections at 1 ps to keep edge on timetool
+                self.atm_fb_pv.put(value = self.correction, timeout = 1.0)  # write to correction PV
+            else:
+                self.atm_fb_pv.put(value = 0.001, timeout = 1.0)
 
 def run():
     correction = drift_correction() # initialize
