abc10dee3a2aee9b0d2b577d5ca934d83f727c3d
rlodico06 <138170352+rlodico06@users.noreply.github.com>
2025-05-28 16:50:29 -0700

    Corrected conversion factors

--- exp-timing/atm_fb_tester.py
+++ exp-timing/atm_fb_tester.py
@@ -1,48 +1,3 @@
-"""
-=======OUTLINE========
-
-Test Cases:
-- Basic Test:
-    - Ensure correct correction direction when inside filter thresholds
-    - Ensure correct correction magnitude when inside filter thresholds
-    - Ensure no correction applied when ampl outside filter thresholds
-    - Ensure filter and error tracking PVs are being written to
-- Advanced Test:
-    - In the tester script, build a function that applies a pseudo-random drift (~80% of drift would be predictable and at a fixed rate, 20% would be "random" fluctuation)
-    - Tester would start an accumulated error tracker (use an UNDS PV so it can be tracked online)
-    - Tester script would monitor dummy UNDS correction PV and subtract the correction from the accumulated error
-
-Process:
-- (Manually) Change ATM correction PV to an UNDS PV so no real correction is applied
-- (Manually) Dump multiple waveform PV datasets to an excel file 
-- (Manually) Run tester script
-- Pull excel data from one dataset into Dicts (separates Dicts for error and ampl)
-- Basic Test
-    - Calculate out the average value of each group of 50 (valid - inside thresholds) error values and save them in a Dict
-    - Save last ampl value in each group of 50 to a dict
-    - Once done calculating averages, prompt user to start other script - await user entry
-    - (Manually) Run correction script
-    - Use a loop to write values from tester Dict to waveform PV at CA speed
-    - Tester script monitors dummy UNDS correction PV - once there is a fresh value, check if it is the same as first calculated value
-    - Also check if tracking PVs have updated:
-        - Check if error tracking PV has the right correction value
-        - Check if ampl tracking PV has the value saved in the ampl dict
-    - Create an array for pass/fail record
-    - At the end of the test, if the array has only passes, report pass
-- Advanced Test
-    - Start a loop that writes pseudo-random drift to waveform PV at CA speed
-    - Start accumulated error tracker and sums the errors that have been written to the waveform PV
-    - Within the loop, check for fresh values in the dummy UNDS correction PV and subtract the correction from the accumulated error
-    - At the end of each loop, if the accumulated error has changed, write it to an UNDS PV for tracking
-    - At the end of the test, print out some stats:
-        - Set rate of drift (hardcoded)
-        - Average residual error
-        - Average residual rate of drift
-        - Highest rate of residual drift at any point during runtime
-        - Maximum accumulated error during runtime
-
-"""
-
 import time
 import random
 from psp.Pv import Pv
@@ -54,12 +9,12 @@ class atm_fb_tester():
         self.data_path = '/cds/home/r/rlodico/scripts/lcls2-atm-fb-test/exp1016923run150.csv'  # path to csv where historical data is saved
         # create PV objects
         self.atm_err_ampl_pv = Pv('LAS:UNDS:FLOAT:59')  # PV to hold dummy edge amplitude for testing
-        self.atm_err_flt_pos_ps_pv = Pv('LAS:UNDS:FLOAT:58')  # PV to hold dummy ps error for testing
+        self.atm_err_flt_pos_fs_pv = Pv('LAS:UNDS:FLOAT:58')  # PV to hold dummy fs error for testing
         self.dummy_fb_pv = Pv('LAS:UNDS:FLOAT:68')  # dummy PV for holding correction values written to by crixs_atm_fb.py
         self.accum_err_pv = Pv('LAS:UNDS:FLOAT:69')  # PV for tracking accumulated error during test
         # connect to PVs
         self.atm_err_ampl_pv.connect(timeout=1.0)
-        self.atm_err_flt_pos_ps_pv.connect(timeout=1.0)
+        self.atm_err_flt_pos_fs_pv.connect(timeout=1.0)
         self.dummy_fb_pv.connect(timeout=1.0)
         self.accum_err_pv.connect(timeout=1.0)
 
@@ -68,9 +23,10 @@ class atm_fb_tester():
         self.accum_err = 0
         self.accum_dict = dict()  # dictionary to hold accum error after each loop
         self.accum_err_pv.put(self.accum_err)  # reset the error accumulator to 0 at start of test
-        self.drift_rate = 0.2  # drift rate in ps/min
+        self.drift_rate = 200  # drift rate in fs/min
+        self.offset = 5198  # real chemRIXS ATM offset in fs
         self.test_duration = 300  # test duration in seconds
-        self.correct_prev = (self.dummy_fb_pv.get(timeout=1.0)) * 1000  # get initial correction value and convert to ps
+        self.correct_prev = (self.dummy_fb_pv.get(timeout=1.0)) * 1000000  # get initial correction value and convert to fs
         self.count = 0
         self.time_prev = time.time()
         self.start_time = time.time()
@@ -79,18 +35,18 @@ class atm_fb_tester():
             self.ampl = random.uniform(20, 80)  # generate random amplitude close to or within the acceptable range
             self.time_elapsed = time.time() - self.time_prev  # calculate seconds since previous correction
             self.time_prev = time.time()  # update previous time for next loop iteration
-            self.fixed_err = (self.drift_rate / 60) * self.time_elapsed  # convert to ps/s, then calculate amount of drift since last loop iteration
+            self.fixed_err = (self.drift_rate / 60) * self.time_elapsed  # convert to fs/s, then calculate amount of drift since last loop iteration
             self.rand_err = random.uniform(-(self.fixed_err * 0.2), (self.fixed_err * 0.2))  # generate a random number less than 20% the magnitude of the fixed error
             self.comb_err = self.fixed_err + self.rand_err  # new error will be fixed error +/- 20%
             # update dummy timetool PVs
             self.atm_err_ampl_pv.put(self.ampl)
             self.curr_err = self.accum_err_pv.get(timeout=1.0)
             self.total_error = self.curr_err + self.comb_err
-            self.atm_err_flt_pos_ps_pv.put(self.total_error)
+            self.atm_err_flt_pos_fs_pv.put(self.total_error + self.offset)  # add in atm offset and write to dummy error PV
             # update error accumulator
             self.accum_err = self.accum_err + self.comb_err
             # if new correction applied, subtract from error accumulator
-            self.correct = (self.dummy_fb_pv.get(timeout=1.0)) * 1000  # convert to ps
+            self.correct = (self.dummy_fb_pv.get(timeout=1.0)) * 1000000  # convert to fs
             if (self.correct != self.correct_prev):
                 self.accum_err = self.accum_err - self.correct
                 self.correct_prev = self.correct
@@ -103,9 +59,9 @@ class atm_fb_tester():
         self.max_accum_err = max(self.accum_dict.values())
         # print test statistics
         print("Test Statistics: ")
-        print("Programmed Drift Rate: ", self.drift_rate, " ps/min")
-        print("Average Residual Error: ", self.avg_accum_err, " ps")
-        print("Maximum Residual Error: ", self.max_accum_err, " ps")
+        print("Programmed Drift Rate: ", self.drift_rate, " fs/min")
+        print("Average Residual Error: ", self.avg_accum_err, " fs")
+        print("Maximum Residual Error: ", self.max_accum_err, " fs")
 
     def data_test(self):
         # test set-up
@@ -127,10 +83,10 @@ class atm_fb_tester():
             self.ampl = self.ampls[self.count]
             self.atm_err_ampl_pv.put[self.ampl]
             # calculate new atm error
-            self.data_err = self.data_errs[self.count]  # raw historical error value
-            self.correct = (self.dummy_fb_pv.get(timeout=1.0)) * 1000  # convert to ps
+            self.data_err = (self.data_errs[self.count])  # raw historical error value
+            self.correct = (self.dummy_fb_pv.get(timeout=1.0)) * 1000000  # convert to fs
             self.accum_err = self.data_err - self.correct  # total current error is the historical edge position minus the net correction applied
-            self.atm_err_flt_pos_ps_pv.put(value=self.total_error, timeout=1.0)
+            self.atm_err_flt_pos_fs_pv.put(value=self.total_error, timeout=1.0)
             # record accumulated error
             self.accum_dict[self.count] = self.accum_err  # add to dict for end of test stats
             self.accum_err_pv.put(self.accum_err)  # update error accumulator PV
@@ -138,7 +94,7 @@ class atm_fb_tester():
             self.count += 1
         # calculate average residual error
         self.avg_accum_err = sum(self.accum_dict.values()) / len(self.accum_dict)
-        print("Average Residual Error: ", self.avg_accum_err, " ps")
+        print("Average Residual Error: ", self.avg_accum_err, " fs")
 
 
 def run():
