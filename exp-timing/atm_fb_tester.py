0d2f3c62742201c8260d6f19fc7ea9241118338e
rlodico06 <138170352+rlodico06@users.noreply.github.com>
2025-05-27 17:07:53 -0700

    Historical data feature

--- exp-timing/atm_fb_tester.py
+++ exp-timing/atm_fb_tester.py
@@ -46,30 +46,31 @@ Process:
 import time
 import random
 from psp.Pv import Pv
+import pandas as pd
+
 
 class atm_fb_tester():
     def __init__(self):
-       # create PV objects
-       self.atm_err_ampl_pv = Pv('LAS:UNDS:FLOAT:59')  # PV to hold dummy edge amplitude for testing
-       self.atm_err_flt_pos_ps_pv = Pv('LAS:UNDS:FLOAT:58')  # PV to hold dummy ps error for testing
-       self.dummy_fb_pv = Pv('LAS:UNDS:FLOAT:68')  # dummy PV for holding correction values written to by crixs_atm_fb.py
-       self.accum_err_pv = Pv('LAS:UNDS:FLOAT:69')  # PV for tracking accumulated error during test
-       # connect to PVs
-       self.atm_err_ampl_pv.connect(timeout = 1.0)
-       self.atm_err_flt_pos_ps_pv.connect(timeout = 1.0)
-       self.dummy_fb_pv.connect(timeout = 1.0)
-       self.accum_err_pv.connect(timeout = 1.0)
+        # create PV objects
+        self.atm_err_ampl_pv = Pv('LAS:UNDS:FLOAT:59')  # PV to hold dummy edge amplitude for testing
+        self.atm_err_flt_pos_ps_pv = Pv('LAS:UNDS:FLOAT:58')  # PV to hold dummy ps error for testing
+        self.dummy_fb_pv = Pv('LAS:UNDS:FLOAT:68')  # dummy PV for holding correction values written to by crixs_atm_fb.py
+        self.accum_err_pv = Pv('LAS:UNDS:FLOAT:69')  # PV for tracking accumulated error during test
+        # connect to PVs
+        self.atm_err_ampl_pv.connect(timeout=1.0)
+        self.atm_err_flt_pos_ps_pv.connect(timeout=1.0)
+        self.dummy_fb_pv.connect(timeout=1.0)
+        self.accum_err_pv.connect(timeout=1.0)
 
-
-    def advanced_test(self):
+    def arbitrary_test(self):
         # test set-up
         self.accum_err = 0
         self.accum_dict = dict()  # dictionary to hold accum error after each loop
-        self.count = 0
         self.accum_err_pv.put(self.accum_err)  # reset the error accumulator to 0 at start of test
         self.drift_rate = 0.2  # drift rate in ps/min
         self.test_duration = 300  # test duration in seconds
-        self.correct_prev = (self.dummy_fb_pv.get(timeout = 1.0)) * 1000  # get initial correction value and convert to ps
+        self.correct_prev = (self.dummy_fb_pv.get(timeout=1.0)) * 1000  # get initial correction value and convert to ps
+        self.count = 0
         self.time_prev = time.time()
         self.start_time = time.time()
         # test loop
@@ -82,13 +83,13 @@ class atm_fb_tester():
             self.comb_err = self.fixed_err + self.rand_err  # new error will be fixed error +/- 20%
             # update dummy timetool PVs
             self.atm_err_ampl_pv.put(self.ampl)
-            self.curr_err = self.accum_err_pv.get(timeout = 1.0)
+            self.curr_err = self.accum_err_pv.get(timeout=1.0)
             self.total_error = self.curr_err + self.comb_err
             self.atm_err_flt_pos_ps_pv.put(self.total_error)
-            # update error accumulator 
+            # update error accumulator
             self.accum_err = self.accum_err + self.comb_err
             # if new correction applied, subtract from error accumulator
-            self.correct = (self.dummy_fb_pv.get(timeout = 1.0)) * 1000 # convert to ps
+            self.correct = (self.dummy_fb_pv.get(timeout=1.0)) * 1000  # convert to ps
             if (self.correct != self.correct_prev):
                 self.accum_err = self.accum_err - self.correct
                 self.correct_prev = self.correct
@@ -105,11 +106,59 @@ class atm_fb_tester():
         print("Average Residual Error: ", self.avg_accum_err, " ps")
         print("Maximum Residual Error: ", self.max_accum_err, " ps")
 
+    def data_test(self):
+        # test set-up
+        self.accum_err = 0
+        self.accum_dict = dict()  # dictionary to hold accum error after each loop
+        self.accum_err_pv.put(self.accum_err)  # reset the error accumulator to 0 at start of test
+        self.test_duration = 300  # test duration in seconds
+        self.count = 0
+        # import data
+        self.df = pd.read_csv('exp1016923run150.csv')  # NEED TO SAVE TO HOME DIRECTORY AND ADD FILE PATH
+        self.ampls = self.df['Amplitude']
+        self.data_errs = self.df['Position']
+        # start test timers
+        self.time_prev = time.time()
+        self.start_time = time.time()
+        # test loop
+        while ((time.time() - self.start_time) < self.test_duration):
+            # update dummy edge amplitude PV
+            self.ampl = self.ampls[self.count]
+            self.atm_err_ampl_pv.put[self.ampl]
+            # calculate new atm error
+            self.data_err = self.data_errs[self.count]  # raw historical error value
+            self.correct = (self.dummy_fb_pv.get(timeout=1.0)) * 1000  # convert to ps
+            self.accum_err = self.data_err - self.correct  # total current error is the historical edge position minus the net correction applied
+            self.atm_err_flt_pos_ps_pv.put(value=self.total_error, timeout=1.0)
+            # record accumulated error
+            self.accum_dict[self.count] = self.accum_err  # add to dict for end of test stats
+            self.accum_err_pv.put(self.accum_err)  # update error accumulator PV
+            time.sleep(3.0)
+            self.count += 1
+        # calculate average residual error
+        self.avg_accum_err = sum(self.accum_dict.values()) / len(self.accum_dict)
+        print("Average Residual Error: ", self.avg_accum_err, " ps")
+
 
 def run():
     test = atm_fb_tester()  # initialize
     time.sleep(0.5)  # make sure all PVs initialized
-    test.advanced_test()
+    while True:
+        print('1 - Arbitrary Drift Test')
+        print('2 - Historical Data Drift Test')
+        choice = input('Enter the integer corresponding to your desired option: ')
+        try:
+            choice = int(choice)
+            if (choice > 0 and choice < 3):
+                break
+            else:
+                print('Invalid choice. Please select a valid option.')
+        except ValueError:
+            print('Invalid choice. Please enter the integer corresponding to your desired option.')
+    if (choice == 1):
+        test.arbitrary_test()
+    else:
+        test.data_test()
 
 
 if __name__ == "__main__":
--- exp-timing/crixs_atm_fb.py
+++ exp-timing/crixs_atm_fb.py
@@ -5,77 +5,80 @@ class drift_correction():
     """Takes the ATM error PV, filters by edge amplitude, and applies an offset via the laser lockers HLA."""
     def __init__(self):
         # create PV objects
-        self.atm_err_pv = Pv('RIX:TIMETOOL:TTALL')  # timetool waveform PV from the DAQ
-        #self.atm_err_ampl_pv = Pv('LAS:UNDS:FLOAT:59')  # PV to hold dummy edge amplitude for testing - COMMENT IF NOT TESTING
-        #self.atm_err_flt_pos_ps_pv = Pv('LAS:UNDS:FLOAT:58')  # PV to hold dummy ps error for testing - COMMENT IF NOT TESTING
+        #self.atm_err_pv = Pv('RIX:TIMETOOL:TTALL')  # timetool waveform PV from the DAQ - COMMENT IF TESTING
+        self.atm_err_ampl_pv = Pv('LAS:UNDS:FLOAT:59')  # PV to hold dummy edge amplitude for testing - COMMENT IF NOT TESTING
+        self.atm_err_flt_pos_ps_pv = Pv('LAS:UNDS:FLOAT:58')  # PV to hold dummy ps error for testing - COMMENT IF NOT TESTING
         self.atm_fb_pv = Pv('LAS:UNDS:FLOAT:68')  # hook for ATM feedback in laser locker HLA - CURRENTLY DUMMY PV FOR TESTING
         self.ampl_pv = Pv('LAS:UNDS:FLOAT:60')  # edge amplitude
         self.flt_pos_ps_pv = Pv('LAS:UNDS:FLOAT:62')  # position in ps
+        self.flt_pos_offset_pv = Pv('LAS:UNDS:FLOAT:57')  # offset in (fs ?) - based on real ATM data
         self.ampl_min_pv = Pv('LAS:UNDS:FLOAT:63')  # minimum allowable edge amplitude for correction
         self.ampl_max_pv = Pv('LAS:UNDS:FLOAT:64')  # maximum allowable edge amplitude for correction
         self.fb_gain_pv = Pv('LAS:UNDS:FLOAT:65')  # gain of feedback loop
         self.sample_size_pv = Pv('LAS:UNDS:FLOAT:66')  # number of edges to average over
         self.on_off_pv = Pv('LAS:UNDS:FLOAT:67')  # PV to turn drift correction on/off
         # connect to PVs
-        self.atm_err_pv.connect(timeout = 1.0)  # COMMENT THIS LINE IF TESTING
-        #self.atm_err_ampl_pv.connect(timeout = 1.0)  # COMMENT THIS LINE IF NOT TESTING
-        #self.atm_err_flt_pos_ps_pv.connect(timeout = 1.0)  # COMMENT THIS LINE IF NOT TESTING
-        self.atm_fb_pv.connect(timeout = 1.0)
-        self.ampl_pv.connect(timeout = 1.0)
-        self.flt_pos_ps_pv.connect(timeout = 1.0)
-        self.ampl_min_pv.connect(timeout = 1.0)
-        self.ampl_max_pv.connect(timeout = 1.0)
-        self.fb_gain_pv.connect(timeout = 1.0)
-        self.sample_size_pv.connect(timeout = 1.0)
-        self.on_off_pv.connect(timeout = 1.0)
+        #self.atm_err_pv.connect(timeout=1.0)  # COMMENT THIS LINE IF TESTING
+        self.atm_err_ampl_pv.connect(timeout=1.0)  # COMMENT THIS LINE IF NOT TESTING
+        self.atm_err_flt_pos_ps_pv.connect(timeout=1.0)  # COMMENT THIS LINE IF NOT TESTING
+        self.atm_fb_pv.connect(timeout=1.0)
+        self.ampl_pv.connect(timeout=1.0)
+        self.flt_pos_ps_pv.connect(timeout=1.0)
+        self.flt_pos_offset_pv.connect(timeout=1.0)
+        self.ampl_min_pv.connect(timeout=1.0)
+        self.ampl_max_pv.connect(timeout=1.0)
+        self.fb_gain_pv.connect(timeout=1.0)
+        self.sample_size_pv.connect(timeout=1.0)
+        self.on_off_pv.connect(timeout=1.0)
 
 
     def correct(self):
         """Takes ATM waveform PV data, applies filtering to detemine valid error values, and applies a correction to laser locker HLA."""
         self.ampl_vals = dict()  # dictionary to hold amplitude values for averaging
         self.error_vals = dict()  # dictionary to hold error values for averaging
-        self.atm_err = self.atm_err_pv.get(timeout = 1.0)  # COMMENT THIS LINE IF TESTING
-        self.flt_pos_ps = self.atm_err[4]  # COMMENT THIS LINE IF TESTING
-        #self.flt_pos_ps = self.atm_err_flt_pos_ps_pv.get(timeout = 1.0)  # initial error - COMMENT THIS LINE IF NOT TESTING
+        #self.atm_err = self.atm_err_pv.get(timeout=1.0)  # COMMENT THIS LINE IF TESTING
+        #self.flt_pos_ps = self.atm_err[4]  # COMMENT THIS LINE IF TESTING
+        self.flt_pos_ps = self.atm_err_flt_pos_ps_pv.get(timeout = 1.0)  # initial error - COMMENT THIS LINE IF NOT TESTING
         self.count = 0  # counter to track number of error values in dict
-        self.sample_size = self.sample_size_pv.get(timeout = 1.0)  # get user-set sample size
+        self.flt_pos_offset = self.flt_pos_offset_pv.get(timeout=1.0)
+        self.sample_size = self.sample_size_pv.get(timeout=1.0)  # get user-set sample size
         # loop for adding error values to dictionary if it meets threshold conditions
         while (self.count < self.sample_size):
             # get current PV values
-            self.atm_err = self.atm_err_pv.get(timeout = 1.0)  # COMMENT THIS LINE IF TESTING
-            #self.atm_err0 = self.atm_err_ampl_pv.get(timeout = 1.0)  # COMMENT THIS LINE IF NOT TESTING
-            #self.atm_err4 = self.atm_err_flt_pos_ps_pv.get(timeout = 1.0)  # COMMENT THIS LINE IF NOT TESTING
-            self.ampl_min = self.ampl_min_pv.get(timeout = 1.0)
-            self.ampl_max = self.ampl_max_pv.get(timeout = 1.0)
+            #self.atm_err = self.atm_err_pv.get(timeout=1.0)  # COMMENT THIS LINE IF TESTING
+            self.atm_err0 = self.atm_err_ampl_pv.get(timeout = 1.0)  # COMMENT THIS LINE IF NOT TESTING
+            self.atm_err4 = self.atm_err_flt_pos_ps_pv.get(timeout = 1.0)  # COMMENT THIS LINE IF NOT TESTING
+            self.ampl_min = self.ampl_min_pv.get(timeout=1.0)
+            self.ampl_max = self.ampl_max_pv.get(timeout=1.0)
             # apply filtering, confirm fresh values, and add to dictionary
-            if (self.atm_err[0] > self.ampl_min) and (self.atm_err[0] < self.ampl_max) and (self.flt_pos_ps != self.atm_err[4]):  # COMMENT THIS LINE IF TESTING
-            #if (self.atm_err0 > self.ampl_min) and (self.atm_err0 < self.ampl_max) and (self.atm_err4 != self.flt_pos_ps):  # COMMENT THIS LINE IF NOT TESTING
-                self.ampl = self.atm_err[0]  # unpack filter parameter - COMMENT THIS LINE IF TESTING
-                self.flt_pos_ps = self.atm_err[4]  # COMMENT THIS LINE IF TESTING
-                #self.ampl = self.atm_err0  # COMMENT THIS LINE IF NOT TESTING
-                #self.flt_pos_ps = self.atm_err4  # COMMENT THIS LINE IF NOT TESTING
+            #if (self.atm_err[0] > self.ampl_min) and (self.atm_err[0] < self.ampl_max) and (self.flt_pos_ps != self.atm_err[4]):  # COMMENT THIS LINE IF TESTING
+            if (self.atm_err0 > self.ampl_min) and (self.atm_err0 < self.ampl_max) and (self.atm_err4 != self.flt_pos_ps):  # COMMENT THIS LINE IF NOT TESTING
+                #self.ampl = self.atm_err[0]  # unpack filter parameter - COMMENT THIS LINE IF TESTING
+                #self.flt_pos_ps = self.atm_err[4] + self.flt_pos_offset  # COMMENT THIS LINE IF TESTING
+                self.ampl = self.atm_err0  # COMMENT THIS LINE IF NOT TESTING
+                self.flt_pos_ps = self.atm_err4 + self.flt_pos_offset  # COMMENT THIS LINE IF NOT TESTING
                 self.ampl_vals[self.count] = self.ampl
                 self.error_vals[self.count] = self.flt_pos_ps
-                self.count+=1
+                self.count += 1
         # averaging
         self.avg_ampl = sum(self.ampl_vals.values()) / len(self.ampl_vals)
         self.avg_error = sum(self.error_vals.values()) / len(self.error_vals)
         # write filter parameter and error value to PVs for easier monitoring
-        self.ampl_pv.put(value = self.avg_ampl, timeout = 1.0)
-        self.flt_pos_ps_pv.put(value = self.avg_error, timeout = 1.0)
+        self.ampl_pv.put(value=self.avg_ampl, timeout=1.0)
+        self.flt_pos_ps_pv.put(value=self.avg_error, timeout=1.0)
         # apply correction
-        self.fb_gain = self.fb_gain_pv.get(timeout = 1.0)  # pull gain PV value
-        self.on_off = self.on_off_pv.get(timeout = 1.0)
-        self.correction = (self.avg_error/1000) * self.fb_gain # scale from ps to ns and apply gain
+        self.fb_gain = self.fb_gain_pv.get(timeout=1.0)  # pull gain PV value
+        self.on_off = self.on_off_pv.get(timeout=1.0)
+        self.correction = (self.avg_error/1000) * self.fb_gain  # scale from ps to ns and apply gain
         if (self.on_off == 1) and ((abs(self.correction) < 0.001)):  # check if drift correction has been turned on and limit corrections to 1 ps
-            self.atm_fb_pv.put(value = self.correction, timeout = 1.0)  # write to correction PV
+            self.atm_fb_pv.put(value=self.correction, timeout=1.0)  # write to correction PV
 
 
 def run():
-    correction = drift_correction() # initialize
+    correction = drift_correction()  # initialize
     try:
         while True:
-            correction.correct() # pull data and filter, then apply correction
+            correction.correct()  # pull data and filter, then apply correction
             time.sleep(1.0)  # keep loop from spinning too fast
     except KeyboardInterrupt:
         print("Script terminated by user.")
