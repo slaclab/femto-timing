dcb954c600cc0ffa108fc96cbbb6503a3899b591
rlodico06 <138170352+rlodico06@users.noreply.github.com>
2025-06-04 18:45:18 -0700

    Pointed back to RIXS waveform PV

--- exp-timing/crixs_atm_fb.py
+++ exp-timing/crixs_atm_fb.py
@@ -5,29 +5,37 @@ class drift_correction():
     """Takes the ATM error PV, filters by edge amplitude, and applies an offset via the laser lockers HLA."""
     def __init__(self):
         # create PV objects
-        #self.atm_err_pv = Pv('RIX:TIMETOOL:TTALL')  # timetool waveform PV from the DAQ - COMMENT IF TESTING
-        self.atm_err_ampl_pv = Pv('LAS:UNDS:FLOAT:59')  # PV to hold dummy edge amplitude for testing - COMMENT IF NOT TESTING
-        self.atm_err_flt_pos_fs_pv = Pv('LAS:UNDS:FLOAT:58')  # PV to hold dummy fs error for testing - COMMENT IF NOT TESTING
+        self.atm_err_pv = Pv('RIX:TIMETOOL:TTALL')  # timetool waveform PV from the DAQ - COMMENT IF TESTING
+        #self.atm_err_ampl_pv = Pv('LAS:UNDS:FLOAT:59')  # PV to hold dummy edge amplitude for testing - COMMENT IF NOT TESTING
+        #self.atm_err_flt_pos_fs_pv = Pv('LAS:UNDS:FLOAT:58')  # PV to hold dummy fs error for testing - COMMENT IF NOT TESTING
         self.atm_fb_pv = Pv('LAS:UNDS:FLOAT:68')  # hook for ATM feedback in laser locker HLA - CURRENTLY DUMMY PV FOR TESTING
-        self.ampl_pv = Pv('LAS:UNDS:FLOAT:60')  # edge amplitude
-        self.flt_pos_fs_pv = Pv('LAS:UNDS:FLOAT:62')  # position in fs
-        self.flt_pos_offset_pv = Pv('LAS:UNDS:FLOAT:57')  # offset in (fs ?) - based on real ATM data
         self.ampl_min_pv = Pv('LAS:UNDS:FLOAT:63')  # minimum allowable edge amplitude for correction
         self.ampl_max_pv = Pv('LAS:UNDS:FLOAT:64')  # maximum allowable edge amplitude for correction
+        self.curr_ampl_pv = Pv('LAS:UNDS:FLOAT:55')  # edge amplitude for the current shot
+        self.ampl_pv = Pv('LAS:UNDS:FLOAT:60')  # average edge amplitude over sample period
+        self.pos_fs_min_pv = Pv('LAS:UNDS:FLOAT:53')  # minimum allowable edge position in fs
+        self.pos_fs_max_pv = Pv('LAS:UNDS:FLOAT:52')  # maximum allowable edge position in fs
+        self.curr_flt_pos_ps_pv = Pv('LAS:UNDS:FLOAT:54')  # position in fs of current edge
+        self.flt_pos_fs_pv = Pv('LAS:UNDS:FLOAT:62')  # average position in fs over sample period
+        self.flt_pos_offset_pv = Pv('LAS:UNDS:FLOAT:57')  # offset in (fs ?) - based on real ATM data
         self.fb_gain_pv = Pv('LAS:UNDS:FLOAT:65')  # gain of feedback loop
         self.sample_size_pv = Pv('LAS:UNDS:FLOAT:66')  # number of edges to average over
         self.on_off_pv = Pv('LAS:UNDS:FLOAT:67')  # PV to turn drift correction on/off
         self.debug_mode_pv = Pv('LAS:UNDS:FLOAT:56')  # PV to turn debug mode on/off
         # connect to PVs
-        #self.atm_err_pv.connect(timeout=1.0)  # COMMENT THIS LINE IF TESTING
-        self.atm_err_ampl_pv.connect(timeout=1.0)  # COMMENT THIS LINE IF NOT TESTING
-        self.atm_err_flt_pos_fs_pv.connect(timeout=1.0)  # COMMENT THIS LINE IF NOT TESTING
+        self.atm_err_pv.connect(timeout=1.0)  # COMMENT THIS LINE IF TESTING
+        #self.atm_err_ampl_pv.connect(timeout=1.0)  # COMMENT THIS LINE IF NOT TESTING
+        #self.atm_err_flt_pos_fs_pv.connect(timeout=1.0)  # COMMENT THIS LINE IF NOT TESTING
         self.atm_fb_pv.connect(timeout=1.0)
-        self.ampl_pv.connect(timeout=1.0)
-        self.flt_pos_fs_pv.connect(timeout=1.0)
-        self.flt_pos_offset_pv.connect(timeout=1.0)
         self.ampl_min_pv.connect(timeout=1.0)
         self.ampl_max_pv.connect(timeout=1.0)
+        self.curr_ampl_pv.connect(timeout=1.0)
+        self.ampl_pv.connect(timeout=1.0)
+        self.pos_fs_min_pv.connect(timeout=1.0)
+        self.pos_fs_max_pv.connect(timeout=1.0)
+        self.curr_flt_pos_ps_pv.connect(timeout=1.0)
+        self.flt_pos_fs_pv.connect(timeout=1.0)
+        self.flt_pos_offset_pv.connect(timeout=1.0)
         self.fb_gain_pv.connect(timeout=1.0)
         self.sample_size_pv.connect(timeout=1.0)
         self.on_off_pv.connect(timeout=1.0)
@@ -38,34 +46,41 @@ class drift_correction():
         """Takes ATM waveform PV data, applies filtering to detemine valid error values, and applies a correction to laser locker HLA."""
         self.ampl_vals = dict()  # dictionary to hold amplitude values for averaging
         self.error_vals = dict()  # dictionary to hold error values for averaging
-        #self.atm_err = self.atm_err_pv.get(timeout=1.0)  # COMMENT THIS LINE IF TESTING
-        #self.flt_pos_fs = self.atm_err[4]  # COMMENT THIS LINE IF TESTING
-        self.flt_pos_fs = self.atm_err_flt_pos_fs_pv.get(timeout = 1.0)  # initial error - COMMENT THIS LINE IF NOT TESTING
+        self.atm_err = self.atm_err_pv.get(timeout=1.0)  # COMMENT THIS LINE IF TESTING
+        self.flt_pos_fs = self.atm_err[4]  # COMMENT THIS LINE IF TESTING
+        #self.flt_pos_fs = self.atm_err_flt_pos_fs_pv.get(timeout = 1.0)  # initial error - COMMENT THIS LINE IF NOT TESTING
         self.count = 0  # counter to track number of error values in dict
         self.flt_pos_offset = self.flt_pos_offset_pv.get(timeout=1.0)
         self.sample_size = self.sample_size_pv.get(timeout=1.0)  # get user-set sample size
+        # pull current filtering thresholds
+        self.ampl_min = self.ampl_min_pv.get(timeout=1.0)
+        self.ampl_max = self.ampl_max_pv.get(timeout=1.0)
+        self.pos_fs_min = self.pos_fs_min_pv.get(timeout=1.0)
+        self.pos_fs_max = self.pos_fs_max_pv.get(timeout=1.0)
         # loop for adding error values to dictionary if it meets threshold conditions
         while (self.count < self.sample_size):
             # get current PV values
-            #self.atm_err = self.atm_err_pv.get(timeout=1.0)  # COMMENT THIS LINE IF TESTING
-            self.atm_err0 = self.atm_err_ampl_pv.get(timeout = 1.0)  # COMMENT THIS LINE IF NOT TESTING
-            self.atm_err4 = self.atm_err_flt_pos_fs_pv.get(timeout = 1.0)  # COMMENT THIS LINE IF NOT TESTING
-            self.ampl_min = self.ampl_min_pv.get(timeout=1.0)
-            self.ampl_max = self.ampl_max_pv.get(timeout=1.0)
+            self.atm_err = self.atm_err_pv.get(timeout=1.0)  # COMMENT THIS LINE IF TESTING
+            #self.atm_err0 = self.atm_err_ampl_pv.get(timeout = 1.0)  # COMMENT THIS LINE IF NOT TESTING
+            #self.atm_err4 = self.atm_err_flt_pos_fs_pv.get(timeout = 1.0)  # COMMENT THIS LINE IF NOT TESTING
+            # update tracking PVs
+            self.curr_ampl_pv.put(value=self.atm_err[0], timeout=1.0)
+            self.curr_flt_pos_ps_pv.put(value=self.atm_err[4] - self.flt_pos_offset, timeout=1.0)
             # apply filtering, confirm fresh values, and add to dictionary
-            #if (self.atm_err[0] > self.ampl_min) and (self.atm_err[0] < self.ampl_max) and (self.atm_err[4] > 3000) and (self.atm_err[4] < 4250) and (self.flt_pos_fs != self.atm_err[4]):  # COMMENT THIS LINE IF TESTING
-            if (self.atm_err0 > self.ampl_min) and (self.atm_err0 < self.ampl_max) and (self.atm_err4 > 3000) and (self.atm_err4 < 4250) and (self.atm_err4 != self.flt_pos_fs):  # COMMENT THIS LINE IF NOT TESTING
-                #self.ampl = self.atm_err[0]  # unpack filter parameter - COMMENT THIS LINE IF TESTING
-                #self.flt_pos_fs = self.atm_err[4] - self.flt_pos_offset  # COMMENT THIS LINE IF TESTING
-                self.ampl = self.atm_err0  # COMMENT THIS LINE IF NOT TESTING
-                self.flt_pos_fs = self.atm_err4 - self.flt_pos_offset  # COMMENT THIS LINE IF NOT TESTING
+            if (self.atm_err[0] > self.ampl_min) and (self.atm_err[0] < self.ampl_max) and (self.atm_err[4] > self.pos_fs_min) and (self.atm_err[4] < self.pos_fs_max) and (self.flt_pos_fs != self.atm_err[4]):  # COMMENT THIS LINE IF TESTING
+            #if (self.atm_err0 > self.ampl_min) and (self.atm_err0 < self.ampl_max) and (self.atm_err4 > 3000) and (self.atm_err4 < 4250) and (self.atm_err4 != self.flt_pos_fs):  # COMMENT THIS LINE IF NOT TESTING
+                self.ampl = self.atm_err[0]  # unpack filter parameter - COMMENT THIS LINE IF TESTING
+                self.flt_pos_fs = self.atm_err[4] - self.flt_pos_offset  # COMMENT THIS LINE IF TESTING
+                #self.ampl = self.atm_err0  # COMMENT THIS LINE IF NOT TESTING
+                #self.flt_pos_fs = self.atm_err4 - self.flt_pos_offset  # COMMENT THIS LINE IF NOT TESTING
+                # add valid amplitudes and edges to dictionary
                 self.ampl_vals[self.count] = self.ampl
                 self.error_vals[self.count] = self.flt_pos_fs
                 self.count += 1
         # averaging
         self.avg_ampl = sum(self.ampl_vals.values()) / len(self.ampl_vals)
         self.avg_error = sum(self.error_vals.values()) / len(self.error_vals)
-        # write filter parameter and error value to PVs for easier monitoring
+        # write average filter parameter and error value to PVs for easier monitoring
         self.ampl_pv.put(value=self.avg_ampl, timeout=1.0)
         self.flt_pos_fs_pv.put(value=self.avg_error, timeout=1.0)
         # apply correction
@@ -76,11 +91,9 @@ class drift_correction():
             self.atm_fb_pv.put(value=self.correction, timeout=1.0)  # write to correction PV
         else:
             self.atm_fb_pv.put(value=0, timeout=1.0)  # if drift correction is turned off, zero out correction value
-        # additional print statements for rapid debugging
+        # additional print statement for rapid debugging
         self.debug_mode = self.debug_mode_pv.get(timeout=1.0)
         if (self.debug_mode == 1):  # keep debug mode turned off when using tester script
-            print('Current amplitude: ', self.atm_err[0])
-            print('Current edge position in fs: ', self.atm_err[0])
             print('Most recent correction value in fs: ', self.correction * 1000000)
 
 
