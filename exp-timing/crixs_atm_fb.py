bc256c0094ab231ad9ba6b3a5d1ab86089cefb81
rlodico06 <138170352+rlodico06@users.noreply.github.com>
2025-06-05 17:00:27 -0700

    Applied scaling to offset

--- exp-timing/crixs_atm_fb.py
+++ exp-timing/crixs_atm_fb.py
@@ -88,7 +88,7 @@ class drift_correction():
         self.on_off = self.on_off_pv.get(timeout=1.0)
         self.correction = (self.avg_error / 1000000) * self.fb_gain  # scale from fs to ns and apply gain
         if (self.on_off == 1) and ((abs(self.correction) < 0.003)):  # check if drift correction has been turned on and limit corrections to 3 ps
-            self.atm_fb_pv.put(value=self.correction + self.flt_pos_offset, timeout=1.0)  # write to correction PV
+            self.atm_fb_pv.put(value=self.correction + (self.flt_pos_offset/1000000), timeout=1.0)  # write to correction PV
         else:
             self.atm_fb_pv.put(value=0, timeout=1.0)  # if drift correction is turned off, zero out correction value
         # additional print statement for rapid debugging
