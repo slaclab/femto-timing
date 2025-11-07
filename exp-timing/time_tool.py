cc1a68e7aa2e16239e0ab95dc20322f4d764ad75
dvc-x <dvc@slac.stanford.edu>
2025-11-07 13:54:47 -0800

    Update time_tool.py

--- exp-timing/time_tool.py
+++ exp-timing/time_tool.py
@@ -1,212 +1,185 @@
-#time_tool.py
+# time_tool.py (pyepics-only)
 import time
 import numpy as np
 import watchdog3
-from psp.Pv import Pv
-import sys
+from epics import PV, caget, caput
+import sys as pysys
+from typing import Dict, Tuple, List
+
+SYSTEMS = {
+    'FS11': dict(TTALL='XCS:TT:01:TTALL', DEV='LAS:FS11:VIT:', STAGE='XPP:LAS:MMN:16.MOVN', IPM='XPP:SB2:BMMON:SUM'),
+    'FS14': dict(TTALL='CXI:TT:01:TTALL', DEV='LAS:FS14:VIT:', STAGE='CXI:LAS:MMN:09.MOVN', IPM='CXI:DG2:BMMON:SUM'),
+    'XPP':  dict(TTALL='XCS:TT:01:TTALL', DEV='LAS:FS3:VIT:',  STAGE='XPP:LAS:MMN:16.MOVN', IPM='XPP:SB2:BMMON:SUM'),
+    'XCS':  dict(TTALL='XCS:TT:01:TTALL', DEV='LAS:FS4:VIT:',  STAGE='XCS:LAS:MMN:01.MOVN', IPM='XCS:SB1:BMMON:SUM'),
+    'MFX':  dict(TTALL='MFX:TT:01:TTALL', DEV='LAS:FS45:VIT:', STAGE='MFX:LAS:MMN:06.MOVN', IPM='MFX:DG2:BMMON:SUM'),
+    'CXI':  dict(TTALL='CXI:TT:01:TTALL', DEV='LAS:FS5:VIT:',  STAGE='CXI:LAS:MMN:09.MOVN', IPM='CXI:DG2:BMMON:SUM'),
+}
+
+TTALL_FIELDS: List[Tuple[int, str]] = [(0, 'Pixel Pos'), (1, 'Edge Position'), (2, 'Amplitude'), (3, '2nd Amplitude'), (4, 'Background ref'), (5, 'FWHM')]
 
 class time_tool():
-    def __init__ (self, sys='NULL'):
-
+    def __init__(self, system='FS14'):
         self.IPM_Threshold = 500.0
         self.Amplitude_Threshold = 0.02
         self.Drift_Adjustment_Threshold = 0.05
         self.FWHM_Threshold_Low = 30.0
         self.FWHM_Threshold_High = 250.0
-        #self.fwhm_threshs: Tuple[float, float] = (30, 130)
         self.Number_Events = 61
-        self.TimeTool_Edges = np.zeros([self.Number_Events])
+        self.TimeTool_Edges = np.zeros(self.Number_Events, dtype=float)
         self.delay = 0.5
 
-        print(f'Starting {sys} ...')
-        if sys == 'FS11': # set up for new bay 1 laser
-            TTALL_Name = 'XPP:TIMETOOL:TTALL'  # time tool array name
-            Dev_Base = 'LAS:FS11:VIT:'  
-            Stage_Name = 'XPP:LAS:MMN:16'  # delay stage for time tool
-            IPM_Name = 'XPP:SB2:BMMON:SUM' # intensity profile monitor PV
-        elif sys == 'FS14':  # set up FS14 system
-            #TTALL_Name = 'TMO:TIMETOOL:TTALL'  # time tool array name
-            Dev_Base = 'LAS:FS14:VIT:'
-            #Stage_Name = 'LM1K4:COM_MP2_DLY1'  # delay stage for time tool
-            #IPM_Name = 'EM2K0:XGMD:HPS:milliJoulesPerPulse' # intensity profile monitor PV
-            print('Borrow PVs to monitor')
-            TTALL_Name = 'XCS:TT:01:TTALL' #time tool array name
-            Stage_Name = 'CXI:LAS:MMN:01'  # delay stage for time tool
-            IPM_Name = 'XCS:SB1:BMMON:SUM' # intensity profile monitor PV
-            #self.IPM_Threshold = 10.0 #500
-            #self.Amplitude_Threshold = 0.01
-        elif sys == 'XPP':  # set up xpp system
-            TTALL_Name = 'XPP:TIMETOOL:TTALL'  # time tool array name
-            Dev_Base = 'LAS:FS3:VIT:'
-            Stage_Name = 'XPP:LAS:MMN:16'  # delay stage for time tool
-            IPM_Name = 'XPP:SB2:BMMON:SUM' # intensity profile monitor PV
-        elif sys == 'XCS':  # set up xcs system
-            TTALL_Name = 'XCS:TIMETOOL:TTALL'  # time tool array name
-            Dev_Base = 'LAS:FS4:VIT:'
-            Stage_Name = 'XCS:LAS:MMN:01'  # delay stage for time tool
-            IPM_Name = 'XCS:SB1:BMMON:SUM' # intensity profile monitor PV
-        elif sys == 'MFX':  # set up xcs system
-            TTALL_Name = 'MFX:TT:01:TTALL'  # time tool array name
-            Dev_Base = 'LAS:FS45:VIT:'
-            Stage_Name = 'MFX:LAS:MMN:06'  # delay stage for time tool
-            IPM_Name = 'MFX:DG2:BMMON:SUM' # intensity profile monitor PV
-        elif sys == 'CXI':  # set up cxi system
-            TTALL_Name = 'CXI:TT:01:TTALL' #time tool array name
-            Dev_Base = 'LAS:FS5:VIT:'
-            Stage_Name = 'CXI:LAS:MMN:09'  # delay stage for time tool
-            #Stage_Name = 'CXI:USR:MMN:01'  # delay stage for time tool
-            IPM_Name = 'CXI:DG2:BMMON:SUM' # intensity profile monitor PV
-        else:
-            print(sys+' Not Found! Exiting')
-            exit()
+        cfg = SYSTEMS.get(system)
+        print(f'Starting {system} ...')
+        if not cfg:
+            print(system + ' Not Found! Exiting')
+            raise ValueError(f'Unknown system: {system}')
 
-        self.TTALL_PV = Pv(TTALL_Name)
-        self.TTALL_PV.connect(timeout=1.0) # connect to pv
-        self.Stage_PV = Pv(Stage_Name)
-        self.Stage_PV.connect(timeout=1.0)
-        self.IPM_PV = Pv(IPM_Name)
-        self.IPM_PV.connect(timeout=1.0)
-        self.TT_Script_EN = Pv(Dev_Base+'matlab:31')
-        self.TT_Script_EN.connect(timeout=1.0) # connect to pv
-        #self.values = dict() # will hold the numbers from the time tool
-        #self.limits = dict() # will hold limits from matlab pvs
-        #self.old_values = dict() # will hold the old values read from matlab
-        self.Drift_Correct = dict()
-        self.Drift_Correct_PV = dict()  # will hold list of IOC pvs
-        self.Name = ['Watchdog', 'pix', 'Edge Position', 'Amplitude', 'amp_second', 'ref', 'FWHM', 'Stage', 'IPM', 'Drift Correction P', 'Drift Correction Value', 'IPM Good?', 'Amplitude Good?', 'FWHM Good?', 'Good TT Measurement?'] #list of internal names
-        self.Drift_Correct_PV[0] = Dev_Base+'WATCHDOG'
-        self.Drift_Correct_PV[1] = Dev_Base+'PIX'
-        self.Drift_Correct_PV[2] = Dev_Base+'FS'
-        self.Drift_Correct_PV[3] = Dev_Base+'AMP'
-        self.Drift_Correct_PV[4] = Dev_Base+'AMP_SEC'
-        self.Drift_Correct_PV[5] = Dev_Base+'REF'
-        self.Drift_Correct_PV[6] = Dev_Base+'FWHM'
-        self.Drift_Correct_PV[7] = Dev_Base+'STAGE'
-        self.Drift_Correct_PV[8] = Dev_Base+'IPM'
-        self.Drift_Correct_PV[9] = Dev_Base+'DRIFT_CORRECT_SIG'
-        self.Drift_Correct_PV[10]= Dev_Base+'DRIFT_CORRECT_VAL'
-        self.Drift_Correct_PV[11]= Dev_Base+'matlab:11'
-        self.Drift_Correct_PV[12]= Dev_Base+'matlab:12'
-        self.Drift_Correct_PV[13]= Dev_Base+'matlab:13'
-        self.Drift_Correct_PV[14]= Dev_Base+'matlab:14'
+        TTALL_Name = cfg['TTALL']
+        Dev_Base   = cfg['DEV']
+        Stage_Name = cfg['STAGE']
+        IPM_Name   = cfg['IPM']
 
-        for n in range(0, len(self.Name)):
-            self.Drift_Correct[self.Name[n]] = [Pv(self.Drift_Correct_PV[n]), Pv(self.Drift_Correct_PV[n]+'.LOW'), Pv(self.Drift_Correct_PV[n]+'.HIGH'), Pv(self.Drift_Correct_PV[n]+'.DESC')]
-            for x in range(0,4):
-                    self.Drift_Correct[self.Name[n]][x].connect(timeout=1.0)  # connnect to all the various PVs.     
-            for x in range(0,3):
-                self.Drift_Correct[self.Name[n]][x].get(ctrl=True, timeout=1.0)
-            self.Drift_Correct[self.Name[n]][3].put(value = self.Name[n], timeout = 1.0)
-        self.W = watchdog3.watchdog(self.Drift_Correct[self.Name[0]][0]) # initialize watchdog   
+        self.TTALL_PV = PV(TTALL_Name); self.TTALL_PV.wait_for_connection(timeout=1.0)
+        self.Stage_PV = PV(Stage_Name); self.Stage_PV.wait_for_connection(timeout=1.0)
+        self.IPM_PV   = PV(IPM_Name);   self.IPM_PV.wait_for_connection(timeout=1.0)
+        self.TT_Script_EN = PV(Dev_Base + 'matlab:31'); self.TT_Script_EN.wait_for_connection(timeout=1.0)
+
+        self.Name = [
+            'Watchdog','Pixel Pos','Edge Position','Amplitude','2nd Amplitude','Background ref',
+            'FWHM','Stage Moving?','IPM','Drift Correction Signal','Drift Correction Value',
+            'IPM Good?','Amplitude Good?','FWHM Good?','Good TT Measurement?'
+        ]
+        bases = {
+            'Watchdog': Dev_Base+'WATCHDOG',
+            'Pixel Pos': Dev_Base+'PIX',
+            'Edge Position': Dev_Base+'FS',
+            'Amplitude': Dev_Base+'AMP',
+            '2nd Amplitude': Dev_Base+'AMP_SEC',
+            'Background ref': Dev_Base+'REF',
+            'FWHM': Dev_Base+'FWHM',
+            'Stage Moving?': Dev_Base+'STAGE',
+            'IPM': Dev_Base+'IPM',
+            'Drift Correction Signal': Dev_Base+'DRIFT_CORRECT_SIG',
+            'Drift Correction Value': Dev_Base+'DRIFT_CORRECT_VAL',
+            'IPM Good?': Dev_Base+'matlab:11',
+            'Amplitude Good?': Dev_Base+'matlab:12',
+            'FWHM Good?': Dev_Base+'matlab:13',
+            'Good TT Measurement?': Dev_Base+'matlab:14',
+        }
+
+        # Store only the main PVs; no DESC/LOW/HIGH
+        self.Drift_Correct: Dict[str, PV] = {}
+        for name in self.Name:
+            base = bases[name]
+            pv = PV(base); pv.wait_for_connection(timeout=1.0)
+            try:
+                pv.get(timeout=1.0)  # optional warm cache
+            except Exception:
+                pass
+            self.Drift_Correct[name] = pv
+
+        # watchdog3 expects a PV-like object; pass PV directly
+        self.W = watchdog3.watchdog(self.Drift_Correct['Watchdog'])
 
     def read_write(self):
+        self.TT_Script_EN.get(timeout=1.0)
+        run = (self.TT_Script_EN.value == 1)
 
-        self.TT_Script_EN.get(ctrl=True, timeout=1.0)
-        Run_TT_Script = 'Enabled' if self.TT_Script_EN.value == 1 else 'Disabled'
-        if self.Drift_Correct[self.Name[0]][0].value % 100 == 0:
-            print(f'{time.strftime("%x %X")} - The Time Tool Script is {Run_TT_Script}')
+        wd_val = self.Drift_Correct['Watchdog'].get(timeout=1.0)
+        try:
+            if int(wd_val) % 100 == 0:
+                print(f'{time.strftime("%x %X")} - The Time Tool Script is {"Enabled" if run else "Disabled"}')
+        except Exception:
+            pass
 
-        # Use this TT Script to Correct Drift?
-        if self.TT_Script_EN.value == 1:
-            Edge_Count: int = 0
-            time_last_good_val: float = time.time()
-            print(f'{time.strftime("%x %X")} - The Time Tool Script is {Run_TT_Script}')
+        if not run:
+            self.TT_Script_EN.put(0, wait=True, timeout=1.0)
+            return
 
-            while Edge_Count < self.Number_Events:
+        edge_count = 0
+        prev_pix_val = 0
+        last_good = wd_times = time.monotonic()
+        print(f'{time.strftime("%x %X")} - The Time Tool Script is Enabled')
 
-                self.TTALL_PV.get(ctrl=True, timeout=1.0) # get TT array data
-                self.Stage_PV.get(ctrl=True, timeout=1.0) # get TT stage position
-                self.IPM_PV.get(ctrl=True, timeout=1.0) # get intensity profile
+        while edge_count < self.Number_Events:
+            ttall = self.TTALL_PV.get(timeout=1.0)
+            stage = self.Stage_PV.get(timeout=1.0)
+            ipm   = self.IPM_PV.get(timeout=1.0)
 
-                for n in range (1,7):
-                    self.Drift_Correct[self.Name[n]][0].put(value = self.TTALL_PV.value[n-1], timeout = 1.0)  # write to matlab PVs
-                self.Drift_Correct[self.Name[7]][0].put(value = self.Stage_PV.value, timeout = 1.0)  # read stage position
-                self.Drift_Correct[self.Name[8]][0].put(value = self.IPM_PV.value, timeout = 1.0) # read/write intensity profile
-                for n in range (1, 11):
-                    self.Drift_Correct[self.Name[n]][0].get(ctrl=True, timeout = 1.0)
+            for idx, name in TTALL_FIELDS:
+                self.Drift_Correct[name].put(float(ttall[idx]), wait=True, timeout=1.0)
+            self.Drift_Correct['Stage Moving?'].put(float(stage), wait=True, timeout=1.0)
+            self.Drift_Correct['IPM'].put(float(ipm), wait=True, timeout=1.0)
 
-                # Good signal in Intensity Profile Monitor?
-                self.Drift_Correct[self.Name[11]][0].put(value=int(self.Drift_Correct[self.Name[8]][0].value > self.IPM_Threshold), timeout=1.0)
-                # Good Amplitude in Time Tool?
-                self.Drift_Correct[self.Name[12]][0].put(value=int(self.Drift_Correct[self.Name[3]][0].value > self.Amplitude_Threshold), timeout=1.0)
-                # Is FWHM Within the Range?
-                self.Drift_Correct[self.Name[13]][0].put(value=int(self.FWHM_Threshold_Low < self.Drift_Correct[self.Name[6]][0].value < self.FWHM_Threshold_High), timeout=1.0)
+            ipm_val  = float(ipm)
+            pix_val  = float(ttall[0])
+            amp_val  = float(ttall[2])
+            fwhm_val = float(ttall[5])
+            stage_moving = bool(stage)  # 1 means moving, 0 means not
 
-                for n in range (8, 15):
-                    self.Drift_Correct[self.Name[n]][0].get(ctrl=True, timeout = 1.0)
+            stage_ok = not stage_moving
+            ipm_ok  = ipm_val  > self.IPM_Threshold
+            amp_ok  = amp_val  > self.Amplitude_Threshold
+            pix_ok  = pix_val != prev_pix_val and pix_val != 0
+            fwhm_ok = self.FWHM_Threshold_Low < fwhm_val < self.FWHM_Threshold_High
 
-                # Is it a Good Measurement?
-                if all(self.Drift_Correct[self.Name[i]][0].value == 1 for i in range(11, 14)):
-                    #print('Good Measurement!')
-                    self.Drift_Correct[self.Name[14]][0].put(value = 1, timeout = 1.0)            
-                    #self.Drift_Correct[self.Name[9]][0].put(value = self.Drift_Correct[self.Name[2]][0].value, timeout = 1.0)
-                    print(f'Good Measurement! - TT Edge position {self.Drift_Correct[self.Name[2]][0].value:.3f} ps')
+            self.Drift_Correct['IPM Good?'].put(int(ipm_ok), wait=True, timeout=1.0)
+            self.Drift_Correct['Amplitude Good?'].put(int(amp_ok), wait=True, timeout=1.0)
+            self.Drift_Correct['FWHM Good?'].put(int(fwhm_ok), wait=True, timeout=1.0)
+            good = pix_ok and ipm_ok and amp_ok and fwhm_ok and stage_ok
+            self.Drift_Correct['Good TT Measurement?'].put(int(good), wait=True, timeout=1.0)
 
-                    #NEED TO CHANGE TO EDGE VALUE, [2]
-                    self.TimeTool_Edges[Edge_Count] = self.Drift_Correct[self.Name[2]][0].value
-                    Edge_Count += 1
-                    time_last_good_val = time.time()
+            if good:
+                edge = float(ttall[1])
+                prev_pix_val = float(ttall[0])
+                print(f'Good Measurement! - TT Edge position {edge:.3f} ps')
+                self.TimeTool_Edges[edge_count] = edge
+                edge_count += 1
+                last_good = time.monotonic()
 
-                else:
-                    self.Drift_Correct[self.Name[14]][0].put(value = 0, timeout = 1.0)
-                    #print('Not a Good Measurement')
+            if time.monotonic() - last_good > 60:
+                print(f"No good measurement over one minute. Status -> IPM:{ipm_ok}, Amp:{amp_ok}, FWHM:{fwhm_ok}, Pix:{pix_ok}, Stage:{stage_ok}")
+                break
+                
+            if time.monotonic() - wd_times > 0.5:
+                self.W.check()
+                wd_times += 0.5
+            time.sleep(0.01)
 
-                if time.time() - time_last_good_val > 60:
-                    print(f'No good measurement over one minute. Check thresholds?')
-                    break
+        # print(f'Edge count = {edge_count}')
+        if edge_count == self.Number_Events:
+            edge_mean = float(np.mean(self.TimeTool_Edges))# - 0.5 # Middle of the time tool is 0.5?
+            # print(f'Edges Array: [{" ".join(f"{e:.3f}" for e in self.TimeTool_Edges)}]')
+            print(f'{"Good" if ipm_ok else "Low"} Signal in IPM: {ipm_val:.3f}')
+            print(f'{"Good" if amp_ok else "Low"} Amplitude in TT: {amp_val:.3f}')
+            print(f'{"Good" if fwhm_ok else "Bad"} FWHM in TT: {fwhm_val:.3f}')
+            print(f'Mean of Edges = {edge_mean:.6f} ps')
 
-            print(f'Edge count = {Edge_Count}')
-            if Edge_Count == self.Number_Events:
-                Edge_Mean = np.mean(self.TimeTool_Edges)
-                print(f'Edges Array: [{" ".join(f"{edge:.3f}" for edge in self.TimeTool_Edges)}]')
+            if abs(edge_mean) > self.Drift_Adjustment_Threshold:
+                old_val = float(self.Drift_Correct['Drift Correction Value'].get(timeout=1.0))
+                p_gain  = float(self.Drift_Correct['Drift Correction Signal'].get(timeout=1.0))
+                print(f'Old Drift Correction value = {old_val:.6f} ps')
+                edge_mean = edge_mean / 1000  # ps -> ns
+                new_val = p_gain * edge_mean + old_val
+                print(f'# New Drift Correction value = {new_val:.3f} ps')
+                # new_val = new_val / 1000  # ps -> ns
+                self.Drift_Correct['Drift Correction Value'].put(new_val, wait=True, timeout=1.0)
+                time.sleep(1)
 
-                IPM_Good = 'Good' if self.Drift_Correct[self.Name[11]][0].value == 1 else 'Low'
-                Amp_Good = 'Good' if self.Drift_Correct[self.Name[12]][0].value == 1 else 'Low'
-                FWHM_Good = 'Good' if self.Drift_Correct[self.Name[13]][0].value == 1 else 'Bad'
-                print(f'{IPM_Good} Signal in IPM: {self.Drift_Correct[self.Name[8]][0].value:.3f}')
-                print(f'{Amp_Good} Amplitude in TT: {self.Drift_Correct[self.Name[3]][0].value:.3f}')
-                print(f'{FWHM_Good} FWHM in TT: {self.Drift_Correct[self.Name[6]][0].value:.3f}')
-
-                print(f'Mean of Edges = {Edge_Mean:.6f} ps')
-                # Is it the Edge value greater than the threshold?
-                if (abs(Edge_Mean) > self.Drift_Adjustment_Threshold):            
-
-                    # Edge_Mean = Edge_Mean * self.Drift_Correct[self.Name[9]][0].value
-
-                    print(f'Old Drift Correction value = {self.Drift_Correct[self.Name[10]][0].value:.6f} ps')
-                    Edge_Mean = Edge_Mean * self.Drift_Correct[self.Name[9]][0].value + self.Drift_Correct[self.Name[10]][0].value
-                    print(f'New Drift Correction value = {Edge_Mean:.3f} ps')
-
-                    self.Drift_Correct[self.Name[10]][0].put(value = Edge_Mean, timeout = 1.0)
-                    # set position of LXT?
-                    # lxt.set_current_position(-float(txt.position))
-
-                print('-----------------------------------------')
-
-        # Do only a single correction for now, disable correction script?
-        self.TT_Script_EN.put(value=0, timeout=1.0)
-        #self.TT_Script_EN.get(ctrl=True, timeout = 1.0)
-
-
-def run():  # just a loop to keep recording         
-    if len(sys.argv) < 2:
-        T = time_tool()  # initialize
-        print('exit initialize')
-    else:
-        T = time_tool(sys.argv[1])
+def run():
+    system = pysys.argv[1] if len(pysys.argv) > 1 else 'FS14'
+    T = time_tool(system)
     print('Enter main loop')
     while T.W.error == 0:
-        T.W.check() # check / update watchdog counter
+        T.W.check()
         time.sleep(T.delay)
         try:
-            T.read_write()  # collects the data
-        except:
-            del T
-            print('Crashed, restarting')
-            T = time_tool() # create again
+            T.read_write()
+        except Exception as e:
+            print(f'Crashed: {e}, restarting')
+            T = time_tool(system)
             if T.W.error:
-                return        
-    pass  
+                return
 
 if __name__ == "__main__":
-   run()
+    run()
