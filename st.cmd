7c0f6b8127ebd3c484058740417c17134d927e03
Stephanie <sfung@slac.stanford.edu>
2025-12-10 16:49:15 -0800

    prep for release
    * includes pcav2cast updated scripts
    * clean up of unused files and IOCs in the st.cmd

--- st.cmd
+++ st.cmd
@@ -1,9 +1,6 @@
 #!/bin/bash
-# As there is no iocBoot directory for the timing python codes,
-# we end up running this with IOC set to the name and
+# IOCs are set to the script name and
 # HUTCH set to the lowercase hutch name
-# py-fstiming-XXX, py-fstiming-tt-XXX, py-fstiming-pcav-XXX are py2.7
-# py-fstiming-cast-XXX is py3
 hutch=`echo $IOC | awk -F- '{print $NF;}' -`
 base=`basename $IOC -$hutch`
 
@@ -27,18 +24,8 @@ case $base in
       export MPLCONFIGDIR=/reg/d/iocData/fstiming
       ;;
    py-fstiming-tt)
-      if [[ $hutch = XCS ]]; then
-         script=atm2las_fs4.py
-      elif [[ $hutch = RIX ]]; then
-         script=${hutch}_time_tool.py
-      else
-         script=time_tool.py
-      fi
+      script=time_tool.py
       export MPLCONFIGDIR=/reg/d/iocData/fstiming-tt
-      ;;
-   py-fstiming-pcav)
-      script=pcav2ttdrift.py
-      export MPLCONFIGDIR=/reg/d/iocData/fstiming-pcav
       ;;
    py-fstiming-cast)
       source /reg/g/pcds/setup/epicsenv-3.14.12.sh
@@ -55,11 +42,4 @@ case $base in
 esac
 
 echo "Running script $script hutch $hutch"
-
-if [[ $script = pcav2ttdrift.py ]]; then 
-   python $script $hutch -T -P || echo "Script exited with code $?"
-else
-   python $script $hutch || echo "Script exited with code $?" 
-fi
-
-
+python $script $hutch || echo "Script exited with code $?"
